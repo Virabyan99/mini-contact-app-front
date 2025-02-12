d88413b9eae844ec4bb8b291dfd5d1cbcb7bbfba
Virabyan99 <gmparstone99@gmail.com>
2025-02-13 03:56:34 +0400

    !

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-[90%] mx-auto`}>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
         <SessionProvider><Header/>{children}</SessionProvider>
       </body>
     </html>
