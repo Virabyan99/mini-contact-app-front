7a14631bf3170d7aa2fa95b0eb6812041d9ccd2a
Virabyan99 <gmparstone99@gmail.com>
2025-02-13 01:50:37 +0400

    edge added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,7 @@
+// frontend/mini-contact-app-front/app/api/auth/[...nextauth]/route.ts
+
+export const runtime = 'edge'; // Specify that this is an edge function
+
 import { handlers } from "@/lib/auth";
 
-export const { GET, POST } = handlers
+export const { GET, POST } = handlers;
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,20 +1,15 @@
 
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
+export const runtime = 'edge'; // Ensure this is at the top
 
-export const runtime = 'edge';
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
 
- 
+// Your NextAuth configuration
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     GitHub({
-        clientId: process.env.NEXT_AUTH_GITHUB_ID || "",
-        clientSecret: process.env.NEXT_AUTH_GITHUB_SECRET || "",
-      }),
+      clientId: process.env.NEXT_AUTH_GITHUB_ID || "",
+      clientSecret: process.env.NEXT_AUTH_GITHUB_SECRET || "",
+    }),
   ],
-})
-
-
-
-
-
+});
